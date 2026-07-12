0bcd67274df11623f0a2d0a639a76198e6edc5ca
Kai Parsons <thmigu.kp@gmail.com>
2026-07-12 10:35:10 -0700

    feat: ppm image generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+const WIDTH: u32 = 256;
+const HEIGHT: u32 = 256;
+
 fn main() {
-    println!("Hello, world!");
+	println!("P3\n{} {}\n255", WIDTH, HEIGHT);
+
+	for j in 0..HEIGHT {
+		for i in 0..WIDTH {
+			let r = i as f32 / (WIDTH - 1) as f32;
+			let g = j as f32 / (HEIGHT - 1) as f32;
+			let b = 0_f32;
+
+			let ir = (r * 255.999) as u32;
+			let ig = (g * 255.999) as u32;
+			let ib = (b * 255.999) as u32;
+
+			println!("{} {} {}", ir, ig, ib);
+		}
+	}
 }
