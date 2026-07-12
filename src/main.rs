023149e0972fd8363e2341f1c2ebe8be8046cbda
Kai Parsons <thmigu.kp@gmail.com>
2026-07-12 10:46:57 -0700

    feat: scanline progress messages

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ const HEIGHT: u32 = 256;
 
 fn main() {
 	println!("P3\n{} {}\n255", WIDTH, HEIGHT);
-
 	for j in 0..HEIGHT {
+		eprint!("\rScanlines remaining: {} \r", HEIGHT - j);
+
 		for i in 0..WIDTH {
 			let r = i as f32 / (WIDTH - 1) as f32;
 			let g = j as f32 / (HEIGHT - 1) as f32;
@@ -17,4 +18,6 @@ fn main() {
 			println!("{} {} {}", ir, ig, ib);
 		}
 	}
+
+	eprintln!("\rDone.                     ");
 }
