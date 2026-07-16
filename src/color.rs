d929afd00b41bb28461998a9337e3fc2109dd801
Kai Parsons <thmigu.kp@gmail.com>
2026-07-15 18:17:46 -0600

    feat: gamma space color correction

--- src/color.rs
+++ src/color.rs
@@ -4,10 +4,22 @@ use crate::interval::Interval;
 
 pub type Color = Vec3;
 
+fn linear_to_gamma(linear_component: f32) -> f32 {
+	if linear_component > 0_f32 {
+		return linear_component.sqrt();
+	}
+
+	0_f32
+}
+
 pub fn write_color(color: Color) {
-	let r = color.x;
-	let g = color.y;
-	let b = color.z;
+	let mut r = color.x;
+	let mut g = color.y;
+	let mut b = color.z;
+
+	r = linear_to_gamma(r);
+	g = linear_to_gamma(g);
+	b = linear_to_gamma(b);
 
 	let intensity = Interval::new(0_f32, 0.999);
 
